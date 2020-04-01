e6099e7e99ebd836479c4fdae1d983a94c545b40
messidagod <joeshakely@gmail.com>
2020-03-31 19:31:14 -0700

    files

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const hello = "Hello teeeeeeeeeeeeee."
+  
+ReactDOM.render(
+    <p>{hello}</p>,
+      document.getElementById("root")
+    );
+    
